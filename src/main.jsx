217ca3b886353faaad00f6e42fe1a59fd9fadcc1
avikeshari <keshari.avinash7@gmail.com>
2026-04-25 01:52:24 +0530

    class component

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Appp from './Appp.jsx'
 
 createRoot(document.getElementById('root')).render(
     <div>
         <App></App>
+        <Appp></Appp>
     </div>
 )
