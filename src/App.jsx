86d6679f8759eec2233a56c3a44ae4166129f850
avikeshari <keshari.avinash7@gmail.com>
2026-04-26 18:08:32 +0530

    UseState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import React from 'react'
 import Header from './components/Header.jsx'
 
-function App(){ 
+/*function App(){ 
   return <div>
     <Header />
+  </div>
+}*/
+
+const App = () => {
+  const [likes,setLikes] = React.useState(0);
+  const hLike = () => {
+    setLikes(likes + 1); //likes++
+  }
+  return <div>
+    <h1>Likes: {likes}</h1>
+    <button onClick = {hLike}>Like</button>
   </div>
 }
 
