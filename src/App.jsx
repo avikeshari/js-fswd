927e0e347e36f4a08ad624fc33202bfe826b78d7
avikeshari <keshari.avinash7@gmail.com>
2026-04-26 18:25:21 +0530

    Class Use Effect

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from './components/Header.jsx'
   return <div>
     <Header />
   </div>
-}*/
+}
 
 const App = () => {
   const [likes,setLikes] = React.useState(0);
@@ -16,6 +16,28 @@ const App = () => {
     <h1>Likes: {likes}</h1>
     <button onClick = {hLike}>Like</button>
   </div>
+}*/
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likes: 0
+    }
+  }
+
+  hLike = () => {
+    this.setState({
+      likes: this.state.likes + 1
+    })
+  }
+
+  render() {
+    return <div>
+      <h1>Likes: {this.state.likes}</h1>
+      <button onClick={this.hLike}>Like</button>
+    </div>
+  }
 }
 
 export default App;
