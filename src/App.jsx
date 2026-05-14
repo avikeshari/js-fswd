31f304f55014ff58e8eefa45a992ca5377671706
avikeshari <keshari.avinash7@gmail.com>
2026-05-15 02:52:16 +0530

    controlled input

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 const App = () => {
 
-    const [email,setEmail]=React.useState("");
-    const [password,setPassword]=React.useState("");
+    const [email,setEmail]=React.useState("rana.harpreet12@gmail.com");
+    const [password,setPassword]=React.useState("qazsedcf");
     const hLogIn = (e) => {
         e.preventDefault();
-        console.log(event.target.email.value);
-        console.log(event.target.password.value);
+        //console.log(e.target.email.value);
+        console.log(email);
+        //console.log(e.target.password.value);
+        console.log(password);
         console.log("Form submitted successfully!");
     }
     return (
@@ -15,11 +17,11 @@ const App = () => {
             <h1>Log In</h1>
             <form onSubmit={hLogIn}>
                 <div>
-                    <input name="email" type="email" placeholder="email..." value={email} onChange={(e) => console.log(e.target.value)} />
+                    <input name="email" type="email" placeholder="email..." value={email} onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 &nbsp;
                 <div>
-                    <input name="password" type="password" placeholder="password..." />
+                    <input name="password" type="password" placeholder="password..." value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 &nbsp;
                 <div>
