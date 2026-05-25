e27dc59882aa9254aa2d7aa4cac405686fa5f962
avikeshari <keshari.avinash7@gmail.com>
2026-05-26 01:53:16 +0530

    list re-rendering using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import axios from 'axios'
 
 const App = () => {
 
-    const [count, setCount] = React.useState(0);
+    //const [count, setCount] = React.useState(0);
+    const [count, setCount] = React.useState([]);
     
     // This code will fetch the recipes from the API every time the component renders, which is not ideal. We can use useEffect to control when the fetch happens.
     /*fetch('https://6a12d11d78d0434e0d5d82d0.mockapi.io/recipes')
@@ -18,22 +20,42 @@ const App = () => {
     });*/
 
     //with empty dependency array, the useEffect will run only once when the component mounts, which is ideal for fetching data from an API.
-    React.useEffect(() => {
+    /*React.useEffect(() => {
         fetch('https://6a12d11d78d0434e0d5d82d0.mockapi.io/recipes')
         .then(response => response.json())
         .then(recipes => console.log(recipes));
-    }, []);
+    }, []);*/
 
     //with dependency array, the useEffect will run every time the count state changes, which is not ideal for fetching data from an API. We can use an empty dependency array to run the useEffect only once when the component mounts.
-    React.useEffect(() => {
+    /*React.useEffect(() => {
         fetch('https://6a12d11d78d0434e0d5d82d0.mockapi.io/recipes')
         .then(response => response.json())
         .then(recipes => console.log(recipes));
-    }, [count]);
+    }, [count]);*/
+
+    //using axios to fetch data from the API, which is a popular library for making HTTP requests in JavaScript. It provides a simple and easy-to-use API for making requests and handling responses.
+    React.useEffect(() => {
+        axios.get('https://6a12d11d78d0434e0d5d82d0.mockapi.io/recipes')
+        .then(response => setCount(response.data));
+    }, []);
+
+    console.log(count);
+
+    /*return <div>
+        <button onClick={() => setCount(count + 1)}>Fetch API</button>
+    </div>*/
 
     return <div>
-        <button onClick={() => setCount(count + 1)}>Fetch API</button>
+        <h1>Menu</h1>
+        <ul>
+            {
+                count.map(recipes => (
+                    <li key={recipes.id}>{recipes.name}</li>
+                ))
+            }
+        </ul>
     </div>
+
 }
 
 export default App
